a6d3c862915bf39313a72d797eb44ab739ee1b86
ProtoBit <admin@protobyte.tech>
2021-01-28 15:22:00 +0000

    Still doesn't work

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 extern crate pancurses;
 
-use pancurses::{initscr, endwin, Input, noecho};
-use std::ptr::null;
-use crate::EntryContents::Menu;
+use pancurses::{initscr, endwin, Input, noecho, resize_term};
+use std::ptr::{null, null_mut};
 
 #[derive(PartialEq, Eq)]
 pub enum BorderType {
@@ -48,77 +47,87 @@ impl Entry {
     }
 }
 
-pub struct ctx <'a,'b> {
+pub struct ctx<'a> {
     title: String,
     bdtype: BorderType,
     entries: Entry,
     window: pancurses::Window,
     requires_redraw: bool,
-    cur_menu: *Entry,
-    cur_entry: i32,
-    last_menus: Vec<*Entry>
+    cur_menu: & 'a mut Entry,
+    cur_entry: usize,
+    last_menus: Vec<&'a mut Entry>
 }
 
-impl ctx {
+impl ctx<'_> {
     pub fn new (title_in: &str, bdtype_in: BorderType) -> ctx {
+        let mut entry = Entry {
+            name: "Root Node".to_string(),
+            entry_contents: EntryContents {
+                menu: Some(vec![]),
+                text: None
+            },
+            filler: false
+        };
        let mut ctx = ctx {
            title: title_in.parse().unwrap(),
            bdtype: bdtype_in,
-           entries: Entry {
-               name: "Root Node".to_string(),
-               entry_contents: Menu(Vec![]),
-               filler: false
-           },
+           entries: entry,
            window: initscr(),
            requires_redraw: true,
-           cur_menu: null(),
+           cur_menu: &mut entry,
            cur_entry: 0,
            last_menus: vec![]
        };
-        ctx.cur_menu = &ctx.entries;
+        ctx.cur_menu = &mut ctx.entries;
         ctx
     }
 
     fn draw_pipe_border(&mut self) {
-        self.window.border("|","|","—","—","┌","┐","└","┘");
+        self.window.border('|','|','—','—','┌','┐','└','┘');
     }
 
     fn draw_white_border(&mut self) {
         self.window.color_set(pancurses::COLOR_WHITE);
-        self.window.border("█","█","█","█","█","█","█","█");
+        self.window.border('█','█','█','█','█','█','█','█');
     }
 
     fn draw_black_border(&mut self) {
         self.window.color_set(pancurses::COLOR_BLACK);
-        self.window.border("█","█","█","█","█","█","█","█");
+        self.window.border('█','█','█','█','█','█','█','█');
     }
 
     fn add_entry(&mut self, ent: Entry) {
-        self.cur_menu.push(ent);
+        (*self.cur_menu).entry_contents.menu.as_mut().unwrap().push(ent);
     }
 
     pub fn add_menu(&mut self, name: &str) {
-        self.add_entry(Entry::new(name,EntryContents::Menu(vec![])));
+        self.add_entry(Entry::new(name,EntryContents {
+            menu: Some(vec![]),
+            text: None
+        }));
     }
 
     pub fn add_option(&mut self, name: &str, default_value: String) {
-        self.add_entry(Entry::new(name,EntryContents::Text(default_value)));
+        self.add_entry(Entry::new(name,EntryContents {
+            text: Some(default_value),
+            menu: None
+        }));
     }
 
     pub fn update(&mut self) {
         if self.requires_redraw {
-            match *self.bdtype {
+            match &self.bdtype {
                 Pipe => self.draw_pipe_border(),
                 Black => self.draw_black_border(),
                 White => self.draw_white_border(),
-                None => {},
+                _ => {},
             }
         }
         let curchar = self.window.getch();
         while curchar != None {
             match curchar.unwrap() {
                 Input::KeyResize => {
-                    self.window.resize_term(0,0);
+                    resize_term(0,0);
                     self.requires_redraw = true;
                 },
                 Input::KeyLeft => {
@@ -126,16 +135,16 @@ impl ctx {
                     self.requires_redraw = true;
                 },
                 Input::KeyRight => {
-                    if self.cur_menu[self.cur_entry].entry_contents.menu.is_some() {
+                    if ((*self.cur_menu).entry_contents.menu.as_ref().unwrap())[self.cur_entry].entry_contents.menu.is_some() {
                         self.last_menus.push(self.cur_menu);
-                        self.cur_menu = *self.cur_entry.entry_contents.menu.is_some();
+                        self.cur_menu = &mut ((*self.cur_menu).entry_contents.menu.unwrap()[self.cur_entry]);
                     }
                     else {
                         println!("Tried to edit entry, but that feature is not implemented yet :P")
                     }
                 },
                 Input::KeyUp => self.cur_entry = 0.max(self.cur_entry-1),
-                Input::KeyDown => self.cur_entry = *self.cur_menu.entry_contents.menu.unwrap().len().min((self.cur_entry + 1) as usize),
+                Input::KeyDown => self.cur_entry = (*self.cur_menu).entry_contents.menu.unwrap().len().min((self.cur_entry + 1) as usize),
 
                 _ => {}
             }
@@ -145,9 +154,15 @@ impl ctx {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+
     #[test]
     fn context_creation() {
-        let ctx = ctx::new("Test",BorderType::None);
+        let ctx = super::ctx::new("Test",super::BorderType::None);
+    }
+
+    #[test]
+    fn border() {
+        let mut ctx = super::ctx::new("Test",super::BorderType::None);
+        ctx.update();
     }
 }
