2c8d35c4a3f69f9d74274ce02be8962812798c63
protobit <admin@protobyte.tech>
2021-01-30 18:17:53 +0000

    Started on option editing, KeyDown now works as expected

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 extern crate pancurses;
-use pancurses::{initscr, endwin, Input, noecho, resize_term};
+use pancurses::{initscr, endwin, Input, noecho, resize_term, echo};
 use slotmap::{DefaultKey, SlotMap};
-use std::borrow::Borrow;
 
 #[derive(PartialEq, Eq)]
 pub enum BorderType {
@@ -32,7 +31,7 @@ impl Entry {
         }
     }
 }
-
+#[allow(non_camel_case_types)]
 pub struct ctx {
     title: String,
     entries: SlotMap<DefaultKey, Entry>,
@@ -41,7 +40,12 @@ pub struct ctx {
     pub cur_menu: DefaultKey,
     pub cur_entry: DefaultKey,
     pub last_menus: Vec<DefaultKey>,
-    pub cur_menu_entry: usize
+    pub cur_menu_entry: usize,
+    root_menu: DefaultKey,
+    pub x_options_offset: u32,
+    pub y_options_offset: u32,
+    pub editing: bool,
+    input_buffer: String,
 }
 
 impl Drop for ctx {
@@ -51,7 +55,7 @@ impl Drop for ctx {
 }
 
 impl ctx {
-    pub fn new (title_in: &str) -> ctx {
+    pub fn new (title_in: &str, x: u32, y: u32) -> ctx {
         let mut entries = SlotMap::new();
         let out = entries.insert(Entry {
             name: "Root Menu".to_string(),
@@ -69,7 +73,12 @@ impl ctx {
             cur_menu: out,
             cur_entry: out,
             last_menus: vec![],
-            cur_menu_entry: 0
+            cur_menu_entry: 0,
+            root_menu: out,
+            x_options_offset: x,
+            y_options_offset: y,
+            editing: false,
+            input_buffer: "".to_string(),
         };
         ctx.window.keypad(true);
         noecho();
@@ -98,55 +107,87 @@ impl ctx {
 
     pub fn update(&mut self) {
         if self.requires_redraw {
+            self.window.clear();
             pancurses::set_title(self.title.as_str());
             self.window.draw_box(0,0);
-            let mut y = 5;
+            let mut y = self.y_options_offset;
             for option_index in self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap() {
                 let option = &self.entries[*option_index];
                 if !option.filler {
-                    if (y-5) == self.cur_menu_entry {
-                        self.window.mvaddstr(y as i32, 5, "[>] ".to_owned()+&self.title);
+                    if option.entry_contents.text.is_some() {
+                        self.window.mvaddstr(y as i32,self.window.get_max_x()-(self.x_options_offset as i32)-(option.entry_contents.text.as_ref().unwrap().len() as i32),option.entry_contents.text.as_ref().unwrap());
+                    }
+                    if (y-self.y_options_offset) == self.cur_menu_entry as u32 {
+                        self.window.mvaddstr(y as i32, self.x_options_offset as i32, "[>] ".to_owned()+option.name.as_str());
                     }
                     else {
-                        self.window.mvaddstr(y as i32, 5, "[ ] ".to_owned()+&self.title);
+                        self.window.mvaddstr(y as i32, self.x_options_offset as i32, "[ ] ".to_owned()+option.name.as_str());
                     }
                     y += 1;
                 }
             }
             self.window.refresh();
+            self.requires_redraw = false;
         }
         let curchar = self.window.getch();
-        match curchar {
-            Some(Input::KeyResize) => {
-                resize_term(0,0);
-                self.requires_redraw = true;
-            },
-            Some(Input::KeyLeft) => {
-                self.cur_menu = self.last_menus.pop().unwrap();
-                self.requires_redraw = true;
-            },
-            Some(Input::KeyRight)=> {
-                if self.entries[self.cur_entry].entry_contents.menu.is_some() {
-                    self.last_menus.push(self.cur_menu);
-                    self.cur_menu = self.cur_entry;
-                }
-                else {
-                    println!("Tried to edit entry, but that feature is not implemented yet :P")
-                }
-            },
-            Some(Input::KeyDown) => {
-                if self.cur_menu_entry < self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap().len() {
-                    self.cur_menu_entry += 1;
-                    self.cur_entry = self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap()[self.cur_menu_entry];
-                }
-            },
-            Some(Input::KeyUp) => {
-                if self.cur_menu_entry != 0 {
-                    self.cur_menu_entry -= 1;
-                    self.cur_entry = self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap()[self.cur_menu_entry];
-                }
-            },
-            _ => {}
+        if !self.editing {
+            match curchar {
+                Some(Input::KeyResize) => {
+                    resize_term(0, 0);
+                    self.requires_redraw = true;
+                },
+                Some(Input::KeyLeft) => {
+                    if self.root_menu != self.cur_menu {
+                        self.cur_menu = self.last_menus.pop().unwrap();
+                        self.requires_redraw = true;
+                    }
+                },
+                Some(Input::KeyRight) => {
+                    if self.entries[self.cur_entry].entry_contents.menu.is_some() {
+                        self.last_menus.push(self.cur_menu);
+                        self.cur_menu = self.cur_entry;
+                        self.requires_redraw = true;
+                    } else {
+                        echo();
+                        self.window.mvinch(self.cur_menu_entry as i32 + (self.y_options_offset as i32),
+                                           self.window.get_max_x() - (self.x_options_offset as i32) -
+                                               (self.entries[self.cur_entry].entry_contents.text.as_ref().unwrap().len() as i32));
+                    }
+                },
+                Some(Input::KeyDown) => {
+                    if self.cur_menu_entry + 1 < self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap().len() {
+                        self.cur_menu_entry += 1;
+                        self.cur_entry = self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap()[self.cur_menu_entry];
+                        self.requires_redraw = true;
+                    }
+                },
+                Some(Input::KeyUp) => {
+                    if self.cur_menu_entry != 0 {
+                        self.cur_menu_entry -= 1;
+                        self.cur_entry = self.entries[self.cur_menu].entry_contents.menu.as_ref().unwrap()[self.cur_menu_entry];
+                        self.requires_redraw = true;
+                    }
+                },
+                _ => {}
+            }
+        }
+        else {
+            match curchar {
+                Some(Input::KeyEnter) => {
+                    self.editing = false;
+                    self.entries[self.cur_entry].entry_contents.text.as_mut().unwrap().clear();
+                    self.entries[self.cur_entry].entry_contents.text.as_mut().unwrap().insert_str(0,self.input_buffer.as_str());
+                    self.input_buffer.clear();
+                },
+                Some(Input::KeyAbort) => {
+                    self.editing = false;
+                    self.input_buffer.clear();
+                },
+                Some(Input::Character(c)) => {
+                    self.input_buffer.push(c);
+                },
+                _ => {},
+            }
         }
     }
 }
